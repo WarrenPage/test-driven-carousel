4b76df87ca17d3b647ba7dea6dae375af46b8c4b
WarrenPage <warrenpage2@gmail.com>
2026-03-09 14:43:48 +0000

    :sparkles: Initial implementation of Carousel component

--- src/Carousel.test.tsx
+++ src/Carousel.test.tsx
@@ -1,5 +1,6 @@
 import { render, screen } from "@testing-library/react";
 import Carousel from "./Carousel";
+import userEvent from "@testing-library/user-event";
 
 describe("Carousel", () => {
     const slides = [
@@ -26,6 +27,36 @@ describe("Carousel", () => {
     it("renders the first slide by default", () => {
         render(<Carousel slides={slides} />);
         const img = screen.getByRole("img");
+        expect(img).toHaveAttribute("src", slides[0].imgUrl);
+    })
+    it("advances the slide when the Next button is clicked", async () => {
+        render(<Carousel slides={slides} />);
+        const img = screen.getByRole("img");
+        const nextButton = screen.getByTestId("next-button");
+        const user = userEvent.setup();
+
+        await user.click(nextButton);
+        expect(img).toHaveAttribute("src", slides[1].imgUrl);
+
+        await user.click(nextButton);
+        expect(img).toHaveAttribute("src", slides[2].imgUrl);
+
+        await user.click(nextButton);
+        expect(img).toHaveAttribute("src", slides[0].imgUrl);
+    })
+    it("reverses the slide when the Prev button is clicked", async () => {
+        render(<Carousel slides={slides} />);
+        const img = screen.getByRole("img");
+        const prevButton = screen.getByTestId("prev-button");
+        const user = userEvent.setup();
+
+        await user.click(prevButton);
+        expect(img).toHaveAttribute("src", slides[2].imgUrl);
+
+        await user.click(prevButton);
+        expect(img).toHaveAttribute("src", slides[1].imgUrl);
+
+        await user.click(prevButton);
         expect(img).toHaveAttribute("src", slides[0].imgUrl);
     })
 });
